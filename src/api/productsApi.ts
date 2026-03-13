b965e6f4efd0a4617d44d49208353db6d9313531
Tanya <79tanka@gmail.com>
2026-03-13 14:54:39 +0200

    added env

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import type { ProductsApiResponse, Product } from '../types';
 
-const API_BASE_URL = 'https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1';
-
+// const API_BASE_URL = 'https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1';
 export const fetchAllProducts = async (): Promise<Product[]> => {
   const response = await axios.get<ProductsApiResponse>('/products', {
     baseURL: API_BASE_URL,
