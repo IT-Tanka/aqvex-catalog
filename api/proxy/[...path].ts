4628987f200367b4219939d48f765a0e55b98d58
Tanya <79tanka@gmail.com>
2026-03-13 22:50:49 +0200

    fix vercel proxy path

---
+++ api/proxy/[...path].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const base =
+    'https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1';
+
+  let path = '';
+
+  if (Array.isArray(req.query.path)) {
+    path = req.query.path.join('/');
+  } else if (typeof req.query.path === 'string') {
+    path = req.query.path;
+  }
+
+  const url = `${base}/${path}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Proxy error:', error);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+}
